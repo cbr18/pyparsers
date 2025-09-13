a576da9b08e50651e02fcf8cfed26bb1ed864c4a
cbr18 <b.r.g.200306@gmail.com>
2025-09-13 14:44:20 +1000

    add redirect

--- datahub/migrations/migrations/000007_create_brands_table.up.sql
+++ datahub/migrations/migrations/000007_create_brands_table.up.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS brands (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(255),
-    ch_name VARCHAR(255),
+    orig_name VARCHAR(255),
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     deleted_at TIMESTAMP WITH TIME ZONE
